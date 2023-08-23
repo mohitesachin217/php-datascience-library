745a04d62d5b5bdb37b48a4beeb1e2d2833553ac
mohitesachin217 <7931096+mohitesachin217@users.noreply.github.com>
2023-08-23 14:35:14 +0530

    Update php_pandas.php
    Test changes

--- php_pandas.php
+++ php_pandas.php
@@ -1,22 +1,69 @@
 <?php 
 
+// Define a class for Series
+class Series {
+    private $data;
+    private $index;
 
+    public function __construct($data, $index) {
+        $this->data = $data;
+        $this->index = $index;
+    }
 
-/**
-*  Php alternative for python pandas 
-*
-*
-*/
+    public function getData() {
+        return $this->data;
+    }
 
+    // Implement basic data manipulation methods
+    public function head($n = 5) {
+        return new Series(array_slice($this->data, 0, $n), array_slice($this->index, 0, $n));
+    }
 
-class php_pandas{
+    public function tail($n = 5) {
+        $startIndex = count($this->data) - $n;
+        return new Series(array_slice($this->data, $startIndex), array_slice($this->index, $startIndex));
+    }
 
-
-
-
-
+    // Implement more methods like mean, sum, etc.
 }
 
+// Define a class for DataFrame
+class DataFrame {
+    private $data;
+    private $index;
+    private $columns;
 
+    public function __construct($data, $index, $columns) {
+        $this->data = $data;
+        $this->index = $index;
+        $this->columns = $columns;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+    // Implement basic data manipulation methods
+    public function select($columns) {
+        // Implement column selection logic
+    }
+
+    public function filter($condition) {
+        // Implement filtering logic
+    }
+
+    // Implement more methods like groupBy, join, etc.
+}
+
+// Usage example
+$data = [10, 20, 30, 40, 50];
+$index = ['A', 'B', 'C', 'D', 'E'];
+
+$series = new Series($data, $index);
+echo "Series Data: " . implode(', ', $series->getData()) . "\n";
+
+// Similar to Pandas, you'd define more methods for data manipulation on Series and DataFrame.
+
+// Note: This is a simplified example. In a real-world scenario, you'd need to handle various data types, error cases, and optimize performance.
 
 ?>
